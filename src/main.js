c67e362949d67087b388385579046b92d7c2055f
JnimyZ <14148604+jnimy-z@user.noreply.gitee.com>
2025-10-22 23:10:19 +0800

    改成CDN引入vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from "vue"; // 从vue3开始使用createApp
-import App from './App.vue';
+// import { createApp } from "vue"; // 从vue3开始使用createApp 这里git环境无法识别vue，所以使用CDN方式引入vue
+// import App from './App.vue';
 
 
-createApp(App).mount('#app');
+// createApp(App).mount('#app');
